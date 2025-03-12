6082ecb83c55b6024b63eac9d1440b9a065a864c
1mgR00T <erkingtujuhbelas@gmail.com>
2025-03-12 20:50:37 +0700

    Update alfa-iran.php

--- alfa-iran.php
+++ alfa-iran.php
@@ -4281,12 +4281,6 @@ function alfaGetDomains($state = false){
 	}
 	return array("lines" => $lines, "state" => $state);
 }
-function alfaCreateParentFolder(){
-	$parent = $GLOBALS['home_cwd'] . "/" . __ALFA_DATA_FOLDER__;
-	if(!@is_dir($parent)){
-		@mkdir($parent, 0755, true);
-	}
-}
 function alfasymlink(){
 alfahead();
 AlfaNum(9,10);
